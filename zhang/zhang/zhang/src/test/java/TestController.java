abb8730ce926e6ed1cfcf3a9d01b796184ec79a0
fantalove699-art <fantalove699@gmail.com>
2026-06-04 23:24:17 +0800

    Upload zhang/zhang/zhang/src/test/java/TestController.java

---
+++ zhang/zhang/zhang/src/test/java/TestController.java
@@ -0,0 +1,9 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "接口正常";
+    }
+}
